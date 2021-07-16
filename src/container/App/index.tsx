ab1dc7925baaf3d918cb706b69c7aa723d8217e1
ezza022 <alfirman.pahlepi@gmail.com>
2021-07-16 11:42:00 +0700

    Header.tsx

---
+++ src/container/App/index.tsx
@@ -0,0 +1,15 @@
+import Header from "../Layout/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="bg-gray-100 mt-14" style={{ height: "300vh" }}>
+        s
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/container/Layout/Header.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import documentIcon from "../../assets/svg/document.svg";
+
+export default function Header() {
+  const [isDropdown, setDropdown] = useState(false);
+  return (
+    <header className="bg-white text-gray-600 w-full flex left-0 top-0 fixed items-center justify-between py-3 px-8">
+      <button className="flex items-center space-x-2">
+        <img src={documentIcon} alt="logo" className="h-10 w-10" />
+        <h1 className="text-2xl">Forms</h1>
+      </button>
+      <div
+        onClick={() => setDropdown(!isDropdown)}
+        className="h-10 w-10 relative rounded-full border-2 border-gray-200 bg-green-600 flex justify-center items-center cursor-pointer"
+      >
+        <span className="text-white text-lg">A</span>
+        <button
+          onClick={(e) => e.stopPropagation()}
+          className={`${
+            !isDropdown && "hidden"
+          } absolute py-2 px-3 -left-5 top-11 border border-gray-300 bg-white rounded shadow-lg hover:bg-indigo-50 outline-none focus:outline-none`}
+        >
+          Logout
+        </button>
+      </div>
+    </header>
+  );
+}
